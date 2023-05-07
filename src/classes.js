e4be97d7609157fdaa8daad3e5719d9f032eb3e6
Steph <stazatorres@hotmail.com>
2023-05-07 10:45:16 +0100

    adding images to buttons

--- src/classes.js
+++ src/classes.js
@@ -1,6 +1,7 @@
 import { body } from "./DOM.js";
 import { expandButtonArray, removeButtonArray } from "./script.js";
 
+
 export class WeatherItem {
   constructor(location, temp, weather, description, icon) {
     this.location = location;
@@ -25,18 +26,34 @@ export class WeatherItem {
     const removeButton = document.createElement("button");
     removeButtonArray.push(removeButton);
     headerDiv.appendChild(removeButton);
-    headerDiv.classList.add('cardHeader')
+    headerDiv.classList.add("cardHeader");
+
+    const cardMiddle = document.createElement("div");
+    weatherCard.appendChild(cardMiddle);
+    cardMiddle.classList.add("cardMiddle");
 
     const image = document.createElement("img");
-    weatherCard.appendChild(image);
+    cardMiddle.appendChild(image);
     image.src = this.icon;
 
     const currentWeather = document.createElement("h3");
-    weatherCard.appendChild(currentWeather);
+    cardMiddle.appendChild(currentWeather);
     currentWeather.innerText = this.weather;
 
+    const footerDiv = document.createElement("div");
+    weatherCard.appendChild(footerDiv);
+    footerDiv.classList.add("cardFooter");
+
     const temperature = document.createElement("h3");
-    weatherCard.appendChild(temperature);
+    footerDiv.appendChild(temperature);
     temperature.innerText = this.temp.toFixed(1); //now a string, also to 1 D.P rounded.
+
+    const expandButton = document.createElement("button");
+    expandButton.setAttribute("type", "button");
+    const expandButtonImage = document.createElement("img");
+    expandButtonImage.setAttribute("src", "/assets/arrow-expand.svg");
+    expandButtonArray.push(expandButton);
+    expandButton.appendChild(expandButtonImage);
+    footerDiv.appendChild(expandButton);
   }
 }
