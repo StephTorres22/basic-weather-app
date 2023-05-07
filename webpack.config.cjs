92487cce2536e7de73883359a71ef4c4b2719925
Steph <stazatorres@hotmail.com>
2023-05-07 12:44:31 +0100

    sorted webpack config, i was struggling with config and getting images to load correctly keeping createWeatherCard inside class so when each new instance is created the method can be called straightaway

--- webpack.config.cjs
+++ webpack.config.cjs
@@ -1,10 +1,21 @@
 const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
   mode: "development",
-  entry: "./src/script.js",
+  entry: {
+    bundle: path.resolve(__dirname, "src/script.js"),
+  },
+  devtool: "inline-source-map",
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "Development",
+      filename: "index.html",
+      template: "./src/index.html",
+    }),
+  ],
   output: {
-    filename: "bundle.js",
+    filename: "[name].bundle.js",
     path: path.resolve(__dirname, "dist"),
   },
   module: {
@@ -20,6 +31,12 @@ module.exports = {
     ],
   },
   devServer: {
-    static: "./dist",
+    static: {
+      directory: path.resolve(__dirname, "src"),
+    },
+  },
+
+  optimization: {
+    runtimeChunk: "single",
   },
 };
