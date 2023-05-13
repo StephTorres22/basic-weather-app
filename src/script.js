085447b56b91be3633415ac385f6b9aeba6d9437
Steph <stazatorres@hotmail.com>
2023-05-13 12:50:17 +0100

    redesign and functionality to display weather for only one location at a time

--- src/script.js
+++ src/script.js
@@ -33,6 +33,7 @@ import {
   createWeatherCard,
   weatherCardArray,
   removeCards,
+  displayCurrentWeather,
 } from "./DOM.js";
 
 const city = document.getElementById("city");
@@ -58,8 +59,6 @@ async function getWeather(place, country = "uk") {
   count++;
   console.log(count);
 
-  console.log[data];
-
   const currentWeather = {};
   currentWeather.location = data.name; //+ ", " + data.sys.country;
   currentWeather.conditon = data.weather[0].main;
@@ -67,11 +66,13 @@ async function getWeather(place, country = "uk") {
   currentWeather.description = data.weather[0].description;
   currentWeather.iconNumber = data.weather[0].icon;
   currentWeather.country = data.sys.country;
+
+  currentWeather.sunrise = data.sys.sunrise;
+  currentWeather.sunset = data.sys.sunset;
   /* 
   displayData(convertUnixToDate(data.sys.sunrise));
   displayData(convertUnixToDate(data.sys.sunset)); */
   return currentWeather;
-  //returning data this way, not as obj{}, means we have access to it outside of this function! very cool
 }
 /* everything will be waiting on this!, does that mean they need to be async functions */
 /* time zone is referenced in seconds +/- of GMT currently uk is 3600, an hour ahead/BST
@@ -85,106 +86,16 @@ function convertUnixToDate(unix) {
   //or toLocalTimeString, just gives actual time
 }
 
-async function createNewWeatherItem(location) {
+async function renderCurrentWeather(location) {
   let data = await getWeather(location);
-  let newWeatherItem = new WeatherItem(
-    data.location,
-    data.temp,
-    data.conditon,
-    data.description,
-    data.iconNumber,
-    data.country
-  );
 
-  //should probably abstract this into a serparate function that takes array and weatherItem as parametres...?
-  //updateCards()//need to change where the cards go!;
-  //checks to see if there are any current searches
-  if (currentSearchResults.length === 0) {
-    currentSearchResults.push(newWeatherItem);
-    newWeatherItem.createWeatherCard();
-  } else {
-    for (let i = 0; i < currentSearchResults.length; i++) {
-      let weatherItem = currentSearchResults[i];
+  console.log("are we doing anything");
 
-
-      //checks for any duplicate locations, probably need to add country to this condition, case Chester
-      if (
-        weatherItem.location == newWeatherItem.location &&
-        weatherItem.country == newWeatherItem.country
-      ) {
-        weatherCardArray.splice(i, 1);
-        currentSearchResults.splice(i, 1, newWeatherItem);
-        //cardHolder.removeChild(targetDiv);
-        removeCards();
-        currentSearchResults.forEach((weatherItem) => {
-          weatherItem.createWeatherCard();
-        });
-      } else {
-        currentSearchResults.push(newWeatherItem);
-        newWeatherItem.createWeatherCard();
-      }
-
-    }
-  }
+  displayCurrentWeather(data.temp, data.conditon, data.iconNumber);
 }
 
-function updateCards() {
-  for (let i = 0; i < currentSearchResults.length; i++) {
-    createNewWeatherItem(currentSearchResults[i].location);
-  }
-}
+renderCurrentWeather("London");
 
-/* async function displayWeather() {
-  for (let i = 0; i < currentSearchResults.length; i++) {
-    let search = currentSearchResults[i];
-    let location = search.location;
-    let temp = search.temp;
-    let weather = search.weather;
-    let icon = search.icon;
-
-    createWeatherCard(location, temp, weather, icon);
-  }
-} */
-
-async function setBackGroundColour() {
-  // const overcasteColour = "rgb(109, 104, 104)";
-  const sunnyColouredIDs = [500, 800, 801];
-  const overcasteColouredIDs = [
-    200, 210, 230, 231, 300, 301, 520, 741, 701, 802, 803, 600, 612, 615, 521,
-    201, 311, 321, 313, 511, 501, 522, 601, 611, 613, 620, 621, 721, 804,
-  ];
-  const gloomyColouredIDs = [
-    202, 211, 212, 221, 232, 302, 312, 314, 502, 503, 504, 531, 602, 616, 622,
-    711, 731, 761, 762, 771, 781,
-  ];
-
-  /* put in try catch handle your errors! */
-  const currentWeatherId = await getCurrentWeatherId();
-
-  if (sunnyColouredIDs.includes(currentWeatherId)) {
-    docuBody.style.backgroundColor = "var(--sunny-colour)";
-  }
-
-  if (gloomyColouredIDs.includes(currentWeatherId)) {
-    docuBody.style.backgroundColor = "var(--gloomy-colour)";
-  }
-
-  if (overcasteColouredIDs.includes(currentWeatherId)) {
-    docuBody.style.backgroundColor = "var(--overcaste-colour)";
-  }
-
-  /* probably won't use this, also maybe a way of using a loop solution to rather than the repetitive if statement. */
-  //could set background colour based of id numbers, store in different arrays and check if id is present in each.
-}
-
-function displayData(data) {
-  let newHeader = document.createElement("h1");
-  document.body.appendChild(newHeader);
-  newHeader.innerText = data;
-}
-
-createNewWeatherItem("London");
-setInterval(updateCards, 60000);
+//setInterval(updateCards, 60000); // turned to half minute for dev
 window.currentSearchResults = currentSearchResults;
-//window.removeButtonArray = removeButtonArray;
-//window.expandButtonArray = expandButtonArray;
+//window.removeButtonArray = removeButtonArray
