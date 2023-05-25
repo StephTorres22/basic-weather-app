aa70ff271232a893c5df239b706bccc7aea629e0
Steph <stazatorres@hotmail.com>
2023-05-25 10:47:39 +0100

    start of expand button implementation

--- src/script.js
+++ src/script.js
@@ -40,12 +40,12 @@ import {
   removeElements,
   currentCoditionsDiv,
   locationTimeDate,
+  createThreeHourIntervalCard,
+  modal,
+  expandButtonArray,
 } from "./DOM.js";
-import {
-  seperateDays,
-  getDayOverviewIcon,
-  getMinMaxTempPerDay,
-} from "./fiveDayForecast.js";
+
+import { seperateDays } from "./fiveDayForecast.js";
 
 const city = document.getElementById("city");
 const getWeatherBtn = document.getElementById("getWeatherBtn");
@@ -64,7 +64,6 @@ getWeatherBtn.addEventListener("click", (e) => {
 
 /* REMEMBER TO DELETE COUNT, ONLY USING TO SEE HOW MANY CALLS ARE MADE TO  */
 export let count = 0;
-
 
 async function getWeather(place, country = "uk") {
   /* try catch, handle errors! */
@@ -105,6 +104,8 @@ function convertUnixToDate(unix) {
   //or toLocalTimeString, just gives actual time
 }
 
+/* RENDER WEATHER DATA FUNCTIONS */
+
 async function renderCurrentWeather(location) {
   let data = await getWeather(location);
   displayCurrentWeather(
@@ -120,6 +121,7 @@ async function renderFiveDayForecast(location) {
   const data = await seperateDays(location);
   /* need to do a full call to recieve complete promise and then split it */
   const daysObjArray = data.minimalData;
+  const intervalObjArray = data.completeData;
 
   daysObjArray.forEach((day, i) => {
     createFiveDayWeatherCard(
@@ -128,6 +130,32 @@ async function renderFiveDayForecast(location) {
       day.minTemp,
       day.maxTemp,
       fiveDayWeatherCardArray[i]
+    );
+  });
+
+  expandButtonArray.forEach((button, index) => {
+    button.addEventListener("click", (e) => {
+     // console.log(index, e.target);
+
+       if (e.target == expandButtonArray[index]) {
+        console.log(index);
+        renderThreeHourIntervals(intervalObjArray, index);
+        modal.showModal();
+      } 
+    });
+  });
+}
+
+function renderThreeHourIntervals(array, index) {
+  array[index].forEach((interval) => {
+    createThreeHourIntervalCard(
+      interval.dt_txt,
+      interval.weather[0].icon,
+      interval.weather[0].main,
+      interval.weather[0].description,
+      interval.main.temp,
+      interval.wind.speed,
+      modal
     );
   });
 }
